39e79b8a5521d5e7665f1255fa0ff65182851c18
Braden Wright <braden.m.wright@gmail.com>
2017-03-27 17:53:18 -0500

    Added git pull

--- scripts/chef_server_upload_all.sh
+++ scripts/chef_server_upload_all.sh
@@ -4,7 +4,7 @@ pushd ~/chef-repo
 
 echo '{"ssl": { "verify": false }}' > ~/.berkshelf/config.json
 
-find ~/chef-repo/cookbooks -maxdepth 1 -type d \( ! -wholename ~/chef-repo/cookbooks \) -exec bash -c "cd '{}' && berks && berks upload" \;
+find ~/chef-repo/cookbooks -maxdepth 1 -type d \( ! -wholename ~/chef-repo/cookbooks \) -exec bash -c "cd '{}' && git pull && berks && berks upload" \;
 
 knife upload /data_bags
 #knife upload /environments
